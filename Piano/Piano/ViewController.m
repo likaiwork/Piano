659b26ee6fb62f5d9e93bfcb34df1c7e25fa36c0
likai <likaiwork@sina.cn>
2016-04-27 22:26:57 +0800

    lk

--- Piano/Piano/ViewController.m
+++ Piano/Piano/ViewController.m
@@ -31,8 +31,11 @@
     _painoMusic = PianoMusic_qiuRiSiYu;
     UIImageView * uiImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"背景.png"]];
     [uiImageView setFrame:CGRectMake(0.0, 0.0, UI_SCREEN_WIDTH, UI_SCREEN_HEIGHT)];
+ 
+    
     [self.view addSubview:uiImageView];
     _whiteView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, UI_SCREEN_WIDTH, UI_SCREEN_HEIGHT)];
+    
     [_whiteView setBackgroundColor:[UIColor clearColor]];
     [self.view addSubview:_whiteView];
     [self addWhiteButton];
@@ -43,12 +46,14 @@
 -(void)addWhiteButton
 {
     float whiteButtonWidth = (UI_SCREEN_WIDTH - 15.0f*2.50f)/14.0f;
+ 
     float blockButtonWith = 27;
     for(int i = 0;i<14;i++)
     {
         UIButton * whiteButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         [whiteButton.layer setCornerRadius:5.0]; //设置矩形四个圆角半径
         [whiteButton setFrame:CGRectMake(2.5+2.5*i+i*whiteButtonWidth, UI_SCREEN_HEIGHT - 110, whiteButtonWidth, 100)];
+        
         [whiteButton setBackgroundImage:[UIImage imageNamed:@"白键.png"] forState:UIControlStateNormal];
         [whiteButton setBackgroundImage:[UIImage imageNamed:@"白键按下.png"] forState:UIControlStateHighlighted];
         [whiteButton addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
@@ -60,6 +65,8 @@
         [blackButton setBackgroundImage:[UIImage imageNamed:@"黑键按下.png"] forState:UIControlStateHighlighted];
         [blackButton addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
         [blackButton setFrame:CGRectMake(whiteButton.right-blockButtonWith/2, UI_SCREEN_HEIGHT - 130, 27, 71)];
+        
+        
         if(i ==0){
             [self.view addSubview:blackButton];
         }else if(i ==1){
