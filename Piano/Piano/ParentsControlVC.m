9be4cb65a8b51c2813b5525d64c59973c9f300d2
likaiwork <likaiwork@sina.cn>
2016-08-02 00:08:49 +0800

    lk

---
+++ Piano/Piano/ParentsControlVC.m
@@ -0,0 +1,37 @@
+//
+//  ParentsControlVC.m
+//  Piano
+//
+//  Created by leiziyun on 16/6/21.
+//  Copyright © 2016年 jordanLi. All rights reserved.
+//
+
+#import "ParentsControlVC.h"
+
+@interface ParentsControlVC ()
+
+@end
+
+@implementation ParentsControlVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
